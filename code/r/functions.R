f3dfbcc6fb012d2bc986c8495ce7c2ba5aa1cd4e
Maikon <mdabati@tereos.com>
2018-12-15 13:54:13 -0200

    Revert "Revert "Fixes #2""
    This reverts commit 5dd021c308322e9cf219d82fdd564d6eb8bd83e8.

---
+++ code/r/functions.R
@@ -0,0 +1,9 @@
+# Funções para obtero triângulo de correlação da matriz
+get_lower_tri<-function(cormat){
+  cormat[upper.tri(cormat)] <- NA
+  return(cormat)
+}
+get_upper_tri <- function(cormat){
+  cormat[lower.tri(cormat)]<- NA
+  return(cormat)
+}
