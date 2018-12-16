3f6becec27db5d88d465ae170de68d6df2d584c3
Maikon Abati <maikon_abati@hotmail.com>
2018-12-16 01:28:20 -0200

    Exclusão do código R
    Mudança total para Python + Spark

--- code/r/functions.R
+++
@@ -1,9 +0,0 @@
-# Funções para obtero triângulo de correlação da matriz
-get_lower_tri<-function(cormat){
-  cormat[upper.tri(cormat)] <- NA
-  return(cormat)
-}
-get_upper_tri <- function(cormat){
-  cormat[lower.tri(cormat)]<- NA
-  return(cormat)
-}
